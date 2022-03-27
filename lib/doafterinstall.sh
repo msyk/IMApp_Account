db4fe9435d24992f29ef28ec99402250e53f1a19
msyk <nii@msyk.net>
2022-03-27 12:40:33 +0900

    update script

--- lib/doafterinstall.sh
+++ lib/doafterinstall.sh
@@ -13,13 +13,12 @@ imRoot="${appRootDir}/vendor/inter-mediator/inter-mediator"
 mkdir -p "${IM_DB_DIR}"
 if [ -e "${IM_DB_FILE}" ]; then
   echo "The database file alread exists as '${IM_DB_FILE}'."
-  /bin/echo -n "If you want to delete the db file and reinstall the db schema, type 'YES'. -->"
-  read choice
-  if [ "${choice}" == "YES" ]; then
+  read -p "If you want to delete the db file and reinstall the db schema, type 'YES'. -->" choice
+  if [ " ${choice}" == " YES" ]; then
     rm "${IM_DB_FILE}"
   else
     echo "The db file didn't touch so far."
-    exit 9
+    exit 0
   fi
 fi
 sqlite3 "${IM_DB_FILE}" < "${myDir}/schema_basic.sql"
