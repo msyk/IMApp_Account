f0a561d187492b3e221c93eede15f1a58eba682b
msyk <nii@msyk.net>
2022-04-02 08:24:05 +0900

    Document and scripts updated.

--- index_contexts.php
+++ index_contexts.php
@@ -35,7 +35,7 @@ IM_Entry(
             'button-names' => ['insert' => '新規会計項目作成'],
             'calculation' => [
                 ['field' => "attached", 'expression' => "if(invoice_path='','','証票有')",],
-                ['field' => "alertStyle", 'expression' => "if(parent_total>0 && item_total!=parent_total,'inline','none')",],
+                ['field' => "alertStyle", 'expression' => "if(parent_total>0 && round(item_total,0)!=round(parent_total,0),'inline','none')",],
                 ['field' => "checkStyle", 'expression' => "if(debit_id=405,'yellow',if(credit_id=141,'papayawhip',if(credit_id=405,'lightyellow',if(debit_id=141,'moccasin',''))))",],
             ],
             'numeric-fields' => ['item_total'],
--- lib/doafterupdate.sh
+++ lib/doafterupdate.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
 
-IM_DB_DIR="~/.im_db"
-IM_DB_FILE="${IM_DB_DIR}/imapp_account.sqlite3"
+DB_PREFIX="imapp_account"
+IM_DB_DIR="${HOME}/.im_db"
+IM_DB_FILE="${IM_DB_DIR}/${DB_PREFIX}.sqlite3"
 myDir=$(
   cd $(dirname "$0")
   pwd
 )
 appRootDir=$(dirname "${myDir}")
 imRoot="${appRootDir}/vendor/inter-mediator/inter-mediator"
+
+sqlite3 "${IM_DB_FILE}" < "${myDir}/schema_views.sql"
+
+echo "**** Please check the README.md file. ****"
